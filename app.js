3f991762669398cd385056b7ee75d13ca9e66e6d
Felipe Matilde <felipematildeita@gmail.com>
2022-06-22 00:55:52 -0300

    Refactoring and add fill rate plus is organic info

--- app.js
+++ app.js
@@ -1,91 +1,107 @@
-// O bizu é https://developer.mapquest.com/documentation/mapquest-js/v1.3/examples/directions-with-multiple-waypoints/
+// I started from https://developer.mapquest.com/documentation/mapquest-js/v1.3/examples/directions-with-multiple-waypoints/
 
-var lixeirasWaypoints = []
+//The following data will be used everywhere
+var binList = [] //List of all bins
+var binWaypoints = [] //List of only those bins that comply with a certain garbage requirement
 var initialScreenLatitude = -23.206344
 var initialScreenLongitude = -45.874893
 var initialScreenZoom = 15
 
-var entradaPortariaLatitude = -23.206368
-var entradaPortariaLongitude = -45.881676
-var saidaPortariaLatitude = -23.206224
-var saidaPortariaLongitude = -45.881520
+var dctaEntranceGateLatitude = -23.206368
+var dctaEntranceGateLongitude = -45.881676
+var dctaExitGateLatitude = -23.206224
+var dctaExigGateLongitude = -45.881520
 
 var mapLeafLet
 
 window.onload = function() {
     L.mapquest.key = config.MY_API_TOKEN;
 
+    ///*
     mapLeafLet = L.mapquest.map('mapId', {
         center: [initialScreenLatitude, initialScreenLongitude],
         layers: L.mapquest.tileLayer('map'),
         zoom: initialScreenZoom
     });
+    //*/
 
-    var preDefinedLixeiraWaypoints = [
-      "-23.204641, -45.876315", //HTO
-      "-23.201778, -45.875185", //H8
-      "-23.200801, -45.874142", //H23A
-      "-23.200152, -45.873808", //H22A
-      "-23.199641, -45.872967", //H17C
-      "-23.199037, -45.872250", //H17B
-      "-23.196622, -45.872856", //H25B
-      "-23.196040, -45.871817", //H25D
-      "-23.195601, -45.868031", //H9C
-      "-23.196066, -45.867493", //H9B
-      "-23.196540, -45.867002", //H9A
-      "-23.200596, -45.865869", //H30D
-      "-23.204550, -45.864042", //H12B
-      "-23.204089, -45.865135", //H11A
-      "-23.202635, -45.869942", //H21A
-      "-23.202888, -45.871385", //H18B
-      "-23.200083, -45.869979" //H27A
+    var preDefinedBinList = [
+      [-23.204641, -45.876315, 70, true], //HTO
+      [-23.201778, -45.875185, 50, false], //H8
+      [-23.200801, -45.874142, 30, true], //H23A
+      [-23.200152, -45.873808, 20, true], //H22A
+      [-23.199641, -45.872967, 20, false], //H17C
+      [-23.199037, -45.872250, 10, true], //H17B
+      [-23.196622, -45.872856, 50, false], //H25B
+      [-23.196040, -45.871817, 100, false], //H25D
+      [-23.195601, -45.868031, 15, false], //H9C
+      [-23.196066, -45.867493, 30, false], //H9B
+      [-23.196540, -45.867002, 10, true], //H9A
+      [-23.200596, -45.865869, 80, true], //H30D
+      [-23.204550, -45.864042, 50, true], //H12B
+      [-23.204089, -45.865135, 49, true], //H11A
+      [-23.202635, -45.869942, 90, false], //H21A
+      [-23.202888, -45.871385, 30, false], //H18B
+      [-23.200083, -45.869979, 15, false] //H27A
     ];
 
-    preDefinedLixeiraWaypoints.forEach((lixeira) => {
-      var latitudeLix = parseFloat(lixeira.split(",")[0])
-      var longitudeLix = parseFloat(lixeira.split(",")[1])
-      drawMarkerAt(latitudeLix,longitudeLix)
+    preDefinedBinList.forEach(bin => {
+      var binLatitude = bin[0]
+      var binLongitude = bin[1]
+      var binFillRate = bin[2]
+      var isOrganicBin = bin[3]
+      drawMarkerAt(binLatitude,binLongitude,binFillRate,isOrganicBin)
+      binList.push(bin)
+      checkIfABinCompliesWithGarbageRequirementAndEventuallyAddItAsAWaypoint(binLatitude,binLongitude,binFillRate,isOrganicBin)
     })
-    preDefinedLixeiraWaypoints.forEach(lixeira=>lixeirasWaypoints.push(lixeira))
 }
 
-function submitEncontrar(event){
+function checkIfABinCompliesWithGarbageRequirementAndEventuallyAddItAsAWaypoint(binLatitude,binLongitude,binFillRate,isOrganicBin){
+  if((isOrganicBin && binFillRate>20) || (!isOrganicBin && binFillRate>40)){
+    binLatitudeAndLongitude = "".concat(binLatitude,",",binLongitude)
+    binWaypoints.push(binLatitudeAndLongitude)
+  }
+}
+
+function findRouteForWaypoints(event){
     event.preventDefault();
     L.mapquest.key = config.MY_API_TOKEN;
 
     L.mapquest.directions().route({
-      start: ''.concat(entradaPortariaLatitude,',',entradaPortariaLongitude),
-      end: ''.concat(saidaPortariaLatitude,',',saidaPortariaLongitude),
-      waypoints: lixeirasWaypoints,
+      start: ''.concat(dctaEntranceGateLatitude,',',dctaEntranceGateLongitude),
+      end: ''.concat(dctaExitGateLatitude,',',dctaExigGateLongitude),
+      waypoints: binWaypoints,
       optimizeWaypoints: true
     });
 }
 
-function submitInserir(event){
+function insertBin(event){
     event.preventDefault();
 
     // getting form data
-    var latitudeLixeira = parseFloat(document.getElementById("latitudeLixeiraId").value);
-    var longitudeLixeira = parseFloat(document.getElementById("longitudeLixeiraId").value);
-
+    var binLatitude = parseFloat(document.getElementById("binLatitudeId").value);
+    var binLongitude = parseFloat(document.getElementById("binLongitudeId").value);
+    var binFillRate = parseFloat(document.getElementById("binFillRateId").value);
+    var isOrganicBin = document.getElementById("isOrganicBinId").checked
+    
     // run directions function
-    drawMarkerAt(latitudeLixeira, longitudeLixeira);
-    var lixeiraWaypoint = ''.concat(latitudeLixeira,`,`,longitudeLixeira)
-    lixeirasWaypoints.push(lixeiraWaypoint)
-    alert(`Added new point! You may enter ${23-lixeirasWaypoints.length} more points!`)
+    drawMarkerAt(binLatitude, binLongitude,binFillRate,isOrganicBin);
+    checkIfABinCompliesWithGarbageRequirementAndEventuallyAddItAsAWaypoint(binLatitude,binLongitude,binFillRate,isOrganicBin)
+    alert(`Added new point! You may enter ${23-binWaypoints.length} more points!`)
 
     // reset form
-    document.getElementById("inserirFormId").reset();
+    document.getElementById("insertBinFormId").reset();
 }
 
-function drawMarkerAt(latitude,longitude){
+function drawMarkerAt(latitude,longitude,fillRate,isOrganicBin){
     var marker = L.marker([latitude,longitude]).addTo(mapLeafLet)
+    //COLOCAR AQUI ALGUM CÓDIGO PARA IMPRIMIR O PREENCHIMENTO E A INFORMAÇÃO SE É ORGÂNICO OU NÃO
 }
 
   // asign the form to form variable
-var inserirButton = document.getElementById('inserirButtonId');
-var encontrarButton = document.getElementById('encontrarButtonId');
+var insertBinButton = document.getElementById('insertBinButtonId');
+var findRouteButton = document.getElementById('findRouteButtonId');
 
 // call the submitForm() function when submitting the form
-inserirButton.addEventListener('click', submitInserir);
-encontrarButton.addEventListener('click', submitEncontrar);
+insertBinButton.addEventListener('click', insertBin);
+findRouteButton.addEventListener('click', findRouteForWaypoints);
