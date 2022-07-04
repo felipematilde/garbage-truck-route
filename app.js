f8c168a42af91be6dd228a01a72012cc4a175632
Felipe Matilde <felipematildeita@gmail.com>
2022-07-04 00:23:31 -0300

    Randomly remove a predefined number of bins

--- app.js
+++ app.js
@@ -3,14 +3,39 @@
 //The following data will be used everywhere
 var binList = [] //List of all bins
 var binWaypoints = [] //List of only those bins that comply with a certain garbage requirement
-var initialScreenLatitude = -23.206344
-var initialScreenLongitude = -45.874893
+var initialScreenLatitude = -23.201186
+var initialScreenLongitude = -45.869375
 var initialScreenZoom = 15
 
 var dctaEntranceGateLatitude = -23.206368
 var dctaEntranceGateLongitude = -45.881676
 var dctaExitGateLatitude = -23.206224
 var dctaExitGateLongitude = -45.881520
+
+var preDefinedBinList = [
+  [-23.204641, -45.876315, 100, true], //1-HTO
+  [-23.201778, -45.875185, 100, false], //2-H8
+  [-23.200801, -45.874142, 100, true], //3-H23A
+  [-23.200152, -45.873808, 100, true], //4-H22A
+  [-23.199641, -45.872967, 100, false], //5-H17C
+  [-23.199037, -45.872250, 100, true], //6-H17B
+  [-23.196622, -45.872856, 100, false], //7-H25B
+  [-23.196040, -45.871817, 100, false], //8-H25D
+  [-23.195601, -45.868031, 100, false], //9-H9C
+  [-23.196066, -45.867493, 100, false], //10-H9B
+  [-23.196540, -45.867002, 100, true], //11-H9A
+  [-23.200596, -45.865869, 100, true], //12-H30D
+  [-23.204550, -45.864042, 100, true], //13-H12B
+  [-23.204089, -45.865135, 100, true], //14-H11A
+  [-23.202635, -45.869942, 100, false], //15-H21A
+  [-23.202888, -45.871385, 100, false], //16-H18B
+  [-23.200083, -45.869979, 100, false], //17-H27A
+  [-23.203455, -45.870205, 100, true], //18-H18A
+  [-23.205159, -45.868896, 100, true], //19-H10E
+  [-23.203641, -45.868346, 100, true], //20-H30D
+  [-23.199024, -45.870427, 100, true], //21-H27C
+  [-23.197164, -45.868747, 100, false] //22-H25B
+];
 
 var mapLeafLet
 
@@ -24,26 +49,6 @@ window.onload = function() {
         zoom: initialScreenZoom
     });
     //*/
-
-    var preDefinedBinList = [
-      [-23.204641, -45.876315, 70, true], //HTO
-      [-23.201778, -45.875185, 50, false], //H8
-      [-23.200801, -45.874142, 30, true], //H23A
-      [-23.200152, -45.873808, 20, true], //H22A
-      [-23.199641, -45.872967, 20, false], //H17C
-      [-23.199037, -45.872250, 10, true], //H17B
-      [-23.196622, -45.872856, 50, false], //H25B
-      [-23.196040, -45.871817, 100, false], //H25D
-      [-23.195601, -45.868031, 15, false], //H9C
-      [-23.196066, -45.867493, 30, false], //H9B
-      [-23.196540, -45.867002, 10, true], //H9A
-      [-23.200596, -45.865869, 80, true], //H30D
-      [-23.204550, -45.864042, 50, true], //H12B
-      [-23.204089, -45.865135, 49, true], //H11A
-      [-23.202635, -45.869942, 90, false], //H21A
-      [-23.202888, -45.871385, 30, false], //H18B
-      [-23.200083, -45.869979, 15, false] //H27A
-    ];
 
     preDefinedBinList.forEach(bin => {
       var binLatitude = bin[0]
@@ -71,18 +76,20 @@ function findRouteForWaypoints(event){
       start: ''.concat(dctaEntranceGateLatitude,',',dctaEntranceGateLongitude),
       end: ''.concat(dctaExitGateLatitude,',',dctaExitGateLongitude),
       waypoints: binWaypoints,
-      optimizeWaypoints: true
-    },directionsCallback)
+      optimizeWaypoints: true,
+      unit: 'k'
+    },findRouteDistanceAndTimeCallbackFunction)
 
-    function directionsCallback(error, response) {
+    function findRouteDistanceAndTimeCallbackFunction(error, response) {
+      let STOP_TIME_s = 30;
     
       var directionsLayer = L.mapquest.directionsLayer({
         directionsResponse: response
       }).addTo(mapLeafLet);
-      console.log(`The distance is ${response.route.distance} miles`)
+      console.log(`The distance is ${1000*response.route.distance}m`)
       console.log(`The route time is ${response.route.time}s`)
-      alert(`The distance is ${response.route.distance}`)
-      alert(`The route time is ${response.route.time}`)
+      alert(`The distance is ${1000*response.route.distance}m`)
+      alert(`The route time is ${response.route.time}s`)
       return mapLeafLet;
     }
 }
@@ -113,7 +120,41 @@ function drawMarkerAt(latitude,longitude,fillRate,isOrganicBin){
   // asign the form to form variable
 var insertBinButton = document.getElementById('insertBinButtonId');
 var findRouteButton = document.getElementById('findRouteButtonId');
+var removePredefinedNumberOfBinsButton = document.getElementById('removePredefinedNumberOfBinsButtonId');
 
 // call the submitForm() function when submitting the form
 insertBinButton.addEventListener('click', insertBin);
 findRouteButton.addEventListener('click', findRouteForWaypoints);
+removePredefinedNumberOfBinsButton.addEventListener('click',removePredefinedNumberOfBinsRandomlyAndFindTheRoute);
+
+function removePredefinedNumberOfBinsRandomlyAndFindTheRoute(event){
+  event.preventDefault();
+  var nOfBinsToBeRemoved = parseInt(document.getElementById("nOfBinsToBeRemovedId").value);
+
+  removePredefinedNumberOfBinsRandomlyFromBinWaypoints(nOfBinsToBeRemoved);
+  findRouteForWaypoints(event);
+}
+
+function removePredefinedNumberOfBinsRandomlyFromBinWaypoints(nOfBinsToBeRemoved){
+  var originalSizeOfWaypointsList = binWaypoints.length;
+  if(nOfBinsToBeRemoved>originalSizeOfWaypointsList){
+    alert("The number of bins to be removed is greater than original list!");
+    return
+  }
+  
+  var listOfRemovedBins = [];
+
+  while(listOfRemovedBins.length <nOfBinsToBeRemoved){
+    var idxToBeRemoved = Math.floor(Math.random()*originalSizeOfWaypointsList);
+    if(!listOfRemovedBins.includes(binWaypoints[idxToBeRemoved])){
+      listOfRemovedBins.push(binWaypoints[idxToBeRemoved]);
+    }
+  }
+
+  binWaypoints = binWaypoints.filter(function(value, index, arr){
+    if(listOfRemovedBins.includes(value)) return false
+    return true
+
+  })
+}
+
